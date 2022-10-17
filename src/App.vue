b2bc1a80f3b430fcdc5183e9f2e6d1421403f76c
suttipong-wisedsa <64350049+suttipong-wisedsa@users.noreply.github.com>
2022-10-17 20:57:05 +0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <h1>ชื่อ:{{firstname}}</h1>
+    <h2>{{lastname}}</h2>
+    <p>:อายุ{{age+age}}</p>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){ // data() ใช้ return ข้อมูล ในรูป Object ไปใช้ใน template
+    return{
+      firstname:"บาส",
+      lastname:"ครับ",
+      age:11
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
